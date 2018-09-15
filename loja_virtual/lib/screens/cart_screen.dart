4ad005c6a4d5db5cf1b5a49de7845ae822ec6aaa
Paulo Henrique <paulohbmatias@cc.ci.ufpb.br>
2018-09-15 19:13:39 -0300

    Adicionado tela de finalização de pedido

--- loja_virtual/lib/screens/cart_screen.dart
+++ loja_virtual/lib/screens/cart_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
+import 'package:loja_virtual/screens/order_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
+import 'package:loja_virtual/widgets/cart_price.dart';
 import 'package:loja_virtual/widgets/discount_card.dart';
+import 'package:loja_virtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 
@@ -74,7 +77,15 @@ class CartScreen extends StatelessWidget {
                       }
                     ).toList(),
                   ),
-                  DiscountCard()
+                  DiscountCard(),
+                  ShipCard(),
+                  CartPrice(() async {
+                    String orderId = await model.finishOrder();
+                    if(orderId != null)
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context)=>OrderScreen(orderId))
+                      );
+                  })
                 ],
               );
             }
